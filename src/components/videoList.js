00834a6624dd2fdf68b0c061e2e3996652b33334
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-08-09 15:37:08 -0500

    simplified

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -2,19 +2,13 @@ import React from 'react'
 
 import VideoListItem from './videoListItem'
 
-const videoBuilder = ({ videos, onVideoSelect }) => {
-  if (!videos) {
-    return <div>Loading...</div>
-  }
-  const videoArr = videos.map((video) => <VideoListItem
-    key={video.etag}
-    video={video}
-    onVideoSelect={onVideoSelect}
-  />)
-
-  return videoArr
-}
-
 export default ({ videos, onVideoSelect }) => <ul className='col-md-4 list-group'>
-  { videoBuilder({ videos, onVideoSelect }) }
+  {
+    videos.map(
+      video => <VideoListItem
+        key={video.etag}
+        video={video}
+        onVideoSelect={onVideoSelect} />
+    )
+  }
 </ul>
