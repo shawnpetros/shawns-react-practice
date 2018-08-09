24d2d9157557a30b68bfa90fb644924778acf754
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-08-09 15:40:33 -0500

    clarity - spacing

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 export default ({ video, onVideoSelect }) => {
   const {
-    thumbnails: { default: { url } },
+    thumbnails: {
+      default: {
+        url
+      }
+    },
     title
   } = video.snippet
 
